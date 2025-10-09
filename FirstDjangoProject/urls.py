8b8ce196e9de66856848f46896699267b8d550bf
Pranaya Shrestha <pranayashrestha299@gmail.com>
2025-10-09 14:15:08 +0545

    Installed DRF

--- FirstDjangoProject/urls.py
+++ FirstDjangoProject/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),  # Include the URLs from the base app
+    path('', include('base.urls')),  
+    path('api/',include('base.api.urls'))# Include the URLs from the base app
     # You can add more apps here as needed
 ]
